ee5e3d8e7137d7700aa11aeaa950ed3327ea27ff
Dominik Gesla <dominikgesla@gmail.com>
2018-04-26 19:38:03 +0200

    initial commit

---
+++ src/Auto.java
@@ -0,0 +1,12 @@
+public class Auto {
+    private String engine;
+    private String radio;
+
+    public String getEngine() {
+        return engine;
+    }
+
+    public void setEngine(String engine) {
+        this.engine = engine;
+    }
+}
